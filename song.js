862465668e30e7ab6d463e8341afbe73600269f7
Allya Fern <allya.fern@gmail.com>
2019-01-05 17:31:24 +0700

    git commit -m "init"

---
+++ song.js
@@ -0,0 +1,21 @@
+/*ES6 Homework 1
+* author: Fern
+* email: allya.fern@gmail.com
+*/
+
+
+// The following codde list attributes of a song call "Bring back my happy weekend, please!"
+
+var title = "Bring back my happy weekend, please!"; 
+var genre = "Blue"; 
+var artist = "Exhausted Developer"; 
+var releaseDate = "Jan 5th, 2019"; 
+var album = "Did he tell you that I'm still a junior developer?"; 
+var label = "The Evil Empire";
+
+console.log(title);
+console.log(genre);
+console.log(artist);
+console.log(releaseDate);
+console.log(album);
+console.log(label);
